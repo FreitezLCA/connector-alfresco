86b605f106ec2e516d396576b42ac70c5f3d49d7
Cesar Freitez <cesar.freitez@chepss.cl>
2023-01-06 21:39:33 +0000

    [ADD] Creación del módulo

---
+++ connector_alfresco/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Conector Odoo - Alfresco',
+    'sumary': """Módulo de conexión entre Odoo y Alfresco""",
+    'description':"""
+        Odoo - Alfresco
+        - Guardar Documentos
+        - Versionar Documentos
+    """,
+    'author': 'CHEPSS',
+    'website': 'https://www.chepss.cl',
+    'category': 'Documentos',
+    'version': '0.1',
+    'depends': [],
+    'license': 'LGPL-3',
+    'data':[
+        
+    ],
+    'demo':[
+        
+    ],
+    
+}
